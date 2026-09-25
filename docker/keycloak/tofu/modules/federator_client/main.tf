9befc39ba016790814d48093f7d5db69afd364f7
Syed Awsaf <syed.awsaf@informed.com>
2026-09-25 20:22:35 +0100

    feat(OSPO): update DBT references to BIST and dependabot configuration

--- docker/keycloak/tofu/modules/federator_client/main.tf
+++ docker/keycloak/tofu/modules/federator_client/main.tf
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # © Crown Copyright 2026. This work has been developed by the National Digital Twin Programme and is legally
-# attributed to the Department for Business and Trade (UK) as the governing entity.
+# attributed to the UK's Department for Business, Innovation, Science and Trade (BIST) as the governing entity.
 
 resource "keycloak_openid_client" "this" {
   realm_id                     = var.realm_id
--- docker/keycloak/tofu/variables.tf
+++ docker/keycloak/tofu/variables.tf
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # © Crown Copyright 2026. This work has been developed by the National Digital Twin Programme and is legally
-# attributed to the Department for Business and Trade (UK) as the governing entity.
+# attributed to the UK's Department for Business, Innovation, Science and Trade (BIST) as the governing entity.
 
 variable "keycloak_url" {
   description = "Keycloak base URL"
